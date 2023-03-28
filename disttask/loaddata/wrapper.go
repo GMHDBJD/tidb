f599faa03a835793650ac5d4238fcae3ed85acd8
gmhdbjd <gmhdbjd@gmail.com>
2023-03-28 22:50:17 +0800

    update

--- disttask/loaddata/wrapper.go
+++ disttask/loaddata/wrapper.go
@@ -22,6 +22,7 @@ import (
 	"github.com/pingcap/tidb/br/pkg/lightning/backend"
 	"github.com/pingcap/tidb/br/pkg/lightning/backend/encode"
 	"github.com/pingcap/tidb/br/pkg/lightning/backend/kv"
+	"github.com/pingcap/tidb/br/pkg/lightning/backend/local"
 	"github.com/pingcap/tidb/br/pkg/lightning/common"
 	"github.com/pingcap/tidb/br/pkg/lightning/config"
 	"github.com/pingcap/tidb/br/pkg/lightning/log"
@@ -33,84 +34,6 @@ import (
 	"github.com/pingcap/tidb/util/intest"
 	"github.com/pingcap/tidb/util/logutil"
 )
-
-func getStore(ctx context.Context, dir string) (storage.ExternalStorage, error) {
-	b, err := storage.ParseBackend(dir, nil)
-	if err != nil {
-		return nil, err
-	}
-	opt := &storage.ExternalStorageOptions{
-		NoCredentials: true,
-	}
-	return storage.New(ctx, b, opt)
-}
-
-func createColumnPermutation(task MinimalTaskMeta) ([]int, error) {
-	var ignoreColumns map[string]struct{}
-	return common.CreateColumnPermutation(task.Table.TargetColumns, ignoreColumns, task.Table.Info, log.Logger{Logger: logutil.BgLogger()})
-}
-
-func buildParser(ctx context.Context, task MinimalTaskMeta) (mydump.Parser, error) {
-	store, err := getStore(ctx, task.Dir)
-	if err != nil {
-		return nil, err
-	}
-	sourceType, err := transformSourceType(task.Format.Type)
-	if err != nil {
-		return nil, err
-	}
-	permutation, err := createColumnPermutation(task)
-	if err != nil {
-		return nil, err
-	}
-
-	cfg := &config.Config{
-		Mydumper: config.MydumperRuntime{
-			ReadBlockSize:          config.ReadBlockSize,
-			CSV:                    task.Format.CSV.Config,
-			DataCharacterSet:       task.Format.DataCharacterSet,
-			DataInvalidCharReplace: task.Format.DataInvalidCharReplace,
-		},
-		TiDB: config.DBStore{
-			SQLMode: task.Format.SQLDump.SQLMode,
-		},
-	}
-	fileMeta := mydump.SourceFileMeta{
-		Type: sourceType,
-		Path: task.Chunk.Path,
-		// only use for parquet
-		FileSize:    task.Chunk.EndOffset,
-		Compression: task.Format.Compression,
-	}
-	chunk := mydump.Chunk{
-		Offset:       task.Chunk.Offset,
-		PrevRowIDMax: task.Chunk.PrevRowIDMax,
-	}
-
-	parser, err := mydump.BuildParser(ctx, cfg, fileMeta, chunk, permutation, nil, store, task.Table.Info)
-	if err != nil {
-		return nil, err
-	}
-	parser.SetLogger(log.Logger{Logger: logutil.BgLogger()})
-	return parser, nil
-}
-
-func buildEncoder(ctx context.Context, task MinimalTaskMeta) (encode.Encoder, error) {
-	idAlloc := kv.NewPanickingAllocators(task.Chunk.PrevRowIDMax)
-	tbl, err := tables.TableFromMeta(idAlloc, task.Table.Info)
-	if err != nil {
-		return nil, err
-	}
-	cfg := &encode.EncodingConfig{
-		SessionOptions: encode.SessionOptions{
-			SQLMode:        task.Format.SQLDump.SQLMode,
-			AutoRandomSeed: task.Chunk.PrevRowIDMax,
-		},
-		Table:  tbl,
-		Logger: log.Logger{Logger: logutil.BgLogger()},
-	}
-	return kv.NewTableKVEncoder(cfg, nil)
-}
 
 func makeTableRegions(ctx context.Context, task *TaskMeta, concurrency int) ([]*mydump.TableRegion, error) {
 	if concurrency <= 0 {
@@ -208,4 +131,83 @@ func openEngine(ctx context.Context, taskMeta *TaskMeta, backend *backend.Backen
 		return nil, err
 	}
 	return engine, nil
+}
+
+func getStore(ctx context.Context, dir string) (storage.ExternalStorage, error) {
+	b, err := storage.ParseBackend(dir, nil)
+	if err != nil {
+		return nil, err
+	}
+	opt := &storage.ExternalStorageOptions{
+		NoCredentials: true,
+	}
+	return storage.New(ctx, b, opt)
+}
+
+func buildEncoder(ctx context.Context, task MinimalTaskMeta) (encode.Encoder, error) {
+	encodeBuilder := local.NewEncodingBuilder(ctx)
+	idAlloc := kv.NewPanickingAllocators(task.Chunk.PrevRowIDMax)
+	tbl, err := tables.TableFromMeta(idAlloc, task.Table.Info)
+	if err != nil {
+		return nil, err
+	}
+	cfg := &encode.EncodingConfig{
+		SessionOptions: encode.SessionOptions{
+			SQLMode:        task.Format.SQLDump.SQLMode,
+			AutoRandomSeed: task.Chunk.PrevRowIDMax,
+		},
+		Table:  tbl,
+		Logger: log.Logger{Logger: logutil.BgLogger()},
+	}
+	return encodeBuilder.NewEncoder(ctx, cfg)
+}
+
+func createColumnPermutation(task MinimalTaskMeta) ([]int, error) {
+	var ignoreColumns map[string]struct{}
+	return common.CreateColumnPermutation(task.Table.TargetColumns, ignoreColumns, task.Table.Info, log.Logger{Logger: logutil.BgLogger()})
+}
+
+func buildParser(ctx context.Context, task MinimalTaskMeta) (mydump.Parser, error) {
+	store, err := getStore(ctx, task.Dir)
+	if err != nil {
+		return nil, err
+	}
+	sourceType, err := transformSourceType(task.Format.Type)
+	if err != nil {
+		return nil, err
+	}
+	permutation, err := createColumnPermutation(task)
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := &config.Config{
+		Mydumper: config.MydumperRuntime{
+			ReadBlockSize:          config.ReadBlockSize,
+			CSV:                    task.Format.CSV.Config,
+			DataCharacterSet:       task.Format.DataCharacterSet,
+			DataInvalidCharReplace: task.Format.DataInvalidCharReplace,
+		},
+		TiDB: config.DBStore{
+			SQLMode: task.Format.SQLDump.SQLMode,
+		},
+	}
+	fileMeta := mydump.SourceFileMeta{
+		Type: sourceType,
+		Path: task.Chunk.Path,
+		// only use for parquet
+		FileSize:    task.Chunk.EndOffset,
+		Compression: task.Format.Compression,
+	}
+	chunk := mydump.Chunk{
+		Offset:       task.Chunk.Offset,
+		PrevRowIDMax: task.Chunk.PrevRowIDMax,
+	}
+
+	parser, err := mydump.BuildParser(ctx, cfg, fileMeta, chunk, permutation, nil, store, task.Table.Info)
+	if err != nil {
+		return nil, err
+	}
+	parser.SetLogger(log.Logger{Logger: logutil.BgLogger()})
+	return parser, nil
 }
